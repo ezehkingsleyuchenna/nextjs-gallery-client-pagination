54c1107a79c0c95a4e48c6e831dac29fd00ed1b3
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-09-01 01:54:48 +0100

    Update pagination-component.tsx

--- src/components/pagination-component.tsx
+++ src/components/pagination-component.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import { Card } from "./ui/card";
+import { faker } from '@faker-js/faker';
+
 export default function PaginationComponent() {
-    return "Hello"
+
+    const image = faker.image.urlPicsumPhotos;
+
+    return (
+        <div>
+            <Card>
+                <Image src={image} alt="" width={640} height={480} />
+            </Card>
+        </div>
+    )
 }
