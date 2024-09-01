7ce1da8654f4e7bf2f8ce48b55ff058505cb87df
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-09-01 01:45:35 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import PaginationComponent from "@/components/pagination-component";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center justify-center w-full p-4 pt-14">
+      <h1 className="text-3xl font-bold tracking-wider leading-6 uppercase text-cyan-900 mb-5">Gallery</h1>
       <PaginationComponent />
     </main>
   );
