9a8341d264230a5a3e540e1e67d53763ffb728e1
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-09-02 07:34:52 +0100

    Update pagination-component.tsx

--- src/components/pagination-component.tsx
+++ src/components/pagination-component.tsx
@@ -6,8 +6,25 @@ import { faker } from '@faker-js/faker';
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious
+} from "@/components/ui/pagination"
+import { cn } from "@/lib/utils";
+
 export default function PaginationComponent() {
     const [images, setImages] = useState<{ image: string }[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPage] = useState(6);
+
+    const lastItemIndex = currentPage * itemsPerPage;
+    const firstItemIndex = lastItemIndex - itemsPerPage;
+    const currentItems = images.slice(firstItemIndex, lastItemIndex);
 
     const generateFakeImage = () => {
         const newImage = faker.image.urlPicsumPhotos();
@@ -24,7 +41,7 @@ export default function PaginationComponent() {
 
         if (storedImages) setImages(JSON.parse(storedImages));
         else {
-            const newImages = Array.from({ length: 15 }, generateFakeImage);
+            const newImages = Array.from({ length: 30 }, generateFakeImage);
             setImages(newImages)
             localStorage.setItem("fakerImages", JSON.stringify(newImages));
         }
@@ -33,9 +50,9 @@ export default function PaginationComponent() {
     return (
         <>
             <Button onClick={ resetLocalStorage }>Reset Local Storage</Button>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-3">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-3 mb-5">
                 {
-                    images.map((item, index) => (
+                    currentItems.map((item, index) => (
                         <Card key={index}>
                             <div className="group flex transform flex-col overflow-hidden transition-all duration-200">
                                 <div className="overflow-hidden rounded-md">
@@ -54,6 +71,79 @@ export default function PaginationComponent() {
                 }
                 
             </div>
+
+            <PaginationSection
+                totalItems={images.length}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
         </>
     )
+}
+
+type PaginationSectionProps = {
+    totalItems: any,
+    itemsPerPage: any,
+    currentPage: any,
+    setCurrentPage: any
+}
+
+function PaginationSection({totalItems, itemsPerPage, currentPage, setCurrentPage}: PaginationSectionProps) {
+    let pages = [];
+    for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+        pages.push(i);        
+    }
+
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < pages.length) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    return (
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => handlePrevPage()} />
+            </PaginationItem>
+
+            {
+                pages.map((page, index) => (
+                    <PaginationItem
+                        key={index}
+                        className={cn({"bg-neutral-100 rounded-md": (currentPage === page)})}
+                    >
+                        <PaginationLink onClick={() => setCurrentPage(page)}>
+                            {page}
+                        </PaginationLink>                        
+                    </PaginationItem>
+                ))
+            }
+            {/* <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem> */}
+            <PaginationItem>
+              <PaginationNext onClick={() => handleNextPage()} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )
 }
